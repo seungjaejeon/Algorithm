b1ab8fb391eb71d08aaf707cc1024f3ba8517dfa
seungjaejeon <wjstmdwo2000@naver.com>
2022-07-03 20:28:55 +0900

    파이썬 에제

---
+++ No.1789.py
@@ -0,0 +1,11 @@
+S=int(input())#S입력받기
+num=[]#자연수들의 리스트
+while(1):#무한루프
+    for i in range(1<4294967295):
+        
+        num.append(i)
+    
+    if(sum(num)==S):#탈출조건
+        break
+
+print(len(num))#리스트의 길이=개수 출력
