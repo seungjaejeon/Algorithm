fbf2b22d4258767374ff67ba5e15a8e6f1b3c5c2
seungjaejeon <wjstmdwo2000@naver.com>
2022-11-28 01:56:01 +0900

    2022_11_28

---
+++ python/week2자료구조/No.2493.py
@@ -0,0 +1,11 @@
+import sys
+N = int(sys.stdin.readline())
+top = list(map(int, sys.stdin.readline().split()))
+l = [top[0]]
+
+for i in range(N):
+    temp = 0
+    temp = top.pop()
+    top
+for i in range(N):
+    print(result[i], end=" ")
