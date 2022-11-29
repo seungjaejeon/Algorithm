d76958c17c102a4ab641805fc9480edff9a57b37
seungjaejeon <wjstmdwo2000@naver.com>
2022-11-30 00:30:58 +0900

    2022_11_30

--- python/week2자료구조/No.5430.py
+++ python/week2자료구조/No.5430.py
@@ -2,20 +2,20 @@ import sys
 from collections import deque
 T = int(sys.stdin.readline())
 for i in range(T):
-    error = 1
-    num = deque()
-    com = str(sys.stdin.readline().rstrip())
-    N = int(sys.stdin.readline())
-    string = str(sys.stdin.readline().rstrip())
-    for j in string:
+    error = 1 # error인지 판단
+    num = deque() # deque
+    com = str(sys.stdin.readline().rstrip()) # 명령어
+    N = int(sys.stdin.readline()) # 개수
+    string = str(sys.stdin.readline().rstrip()) # 입력값
+    for j in string: # 숫자만 num에 저장
         if ord(j) == 91 or ord(j) == 93 or ord(j) == 44:
             continue
         else:
             num.append(j)
-    for k in com:
-        if k == "R":
+    for k in com: # 명령어 해석
+        if k == "R": # R이면 리버스
             num.reverse()
-        if k == "D":
+        if k == "D": # D이면 num이 존재한다면 popleft 없다면 error = 0
             if len(num) == 0 :
                 error = 0
             else: num.popleft()
