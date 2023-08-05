fb9014d737aebdc8ec5520d5c9ce7940c1f0a8b0
seungjaejeon <wjstmdwo2000@naver.com>
2023-08-05 23:46:28 +0900

    15686치킨배달

--- 삼성SW역량테스트기출/15686치킨배달.py
+++ 삼성SW역량테스트기출/15686치킨배달.py
@@ -2,7 +2,6 @@ import sys
 from itertools import combinations
 N, M = map(int, sys.stdin.readline().split())
 pan = [list(map(int, sys.stdin.readline().split())) for _ in range(N)] 
-
 # 각 치킨집의 치킨거리 구하기
 # 오름차순 정렬 후 M개까지 선택 itertool.combinations
 # 나머지 폐업
@@ -14,7 +13,6 @@ for i, pan_ in enumerate(pan):
             ch.append((i,j))
         if value == 1:
             home.append((i,j))
-ch_distance = [[] for i in range(len(home))]
 min_city_distance = float('inf') # 도시의 치킨 거리 저장할 변수
 for chs in combinations(ch,M): # M개의 치킨집 조합으로 선택
     city_distance = 0 # 현재 선택한 M개의 치킨집으로 도시의 치킨 거리를 저장할 변수
@@ -27,5 +25,4 @@ for chs in combinations(ch,M): # M개의 치킨집 조합으로 선택
         city_distance += min_distance # 최소거리를 도시의 치킨 거리에 합함
     if min_city_distance>city_distance: # M개를 선택할 때마다 가장 작은 도시 치킨 거리를 저장함
         min_city_distance=city_distance
-
 print(min_city_distance) #출력
