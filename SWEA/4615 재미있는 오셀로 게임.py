46f276d6feaa31dee25f55698fc81cadc8648388
seungjaejeon <wjstmdwo2000@naver.com>
2023-09-22 19:01:52 +0900

    SWEA 4615 재미있는 오셀로 게임(수정완료)

--- SWEA/4615 재미있는 오셀로 게임.py
+++ SWEA/4615 재미있는 오셀로 게임.py
@@ -1,22 +1,21 @@
 T = int(input())
-
 dx = [0,0,1,1,-1,-1,1,-1]
 dy = [1,-1,0,-1,0,1,1,-1] # 상하좌우대각선
-
 def change(a, b, color, d):
+    global flag # d 방향에 같은 컬러가 있는지 확인하는 flag
     na = a + dx[d]
     nb = b + dy[d]
     if na < 0 or nb < 0 or na >=N or nb >= N:
         return
     if pan[na][nb] == color:
+        flag = True
         return
     if pan[na][nb] != 0 and pan[na][nb] != color:
-        pan[na][nb] = color
+        willchange.append((na,nb))
         change(na, nb, color, d)
-    
-                
-    
+
 for test_case in range(1, T + 1):
+    black, white = 0,0
     N, M = map(int, input().split())
     pan = [[0 for _ in range(N)] for i in range(N)]
     pan[N//2-1][N//2-1] = 2
@@ -27,6 +26,17 @@ for test_case in range(1, T + 1):
         y, x, color = map(int, input().split()) # color == 1? black/ color == 2? white
         pan[x-1][y-1] = color
         for j in range(8):
-        	change(x-1, y-1, color, j)
-        for t in range(N):
-            print(pan[t])
+            flag = False
+            willchange = []
+            change(x-1, y-1, color, j)
+            if flag==True:
+                for t in willchange:
+                    pan[t[0]][t[1]] = color
+    for i in range(N):
+        for j in range(N):
+            if pan[i][j] == 1:
+                black += 1
+            elif pan[i][j] == 2:
+                white += 1
+    print(f'#{test_case} {black} {white}')
+                
