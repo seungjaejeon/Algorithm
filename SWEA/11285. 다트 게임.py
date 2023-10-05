351537c487d144059bc7bf99d8167eebdaf98a36
seungjaejeon <wjstmdwo2000@naver.com>
2023-10-05 10:07:04 +0900

    SWEA 11285 다트게임

--- SWEA/11285. 다트 게임.py
+++ SWEA/11285. 다트 게임.py
@@ -2,13 +2,16 @@ T = int(input())
 for test_case in range(1, T + 1):
     N = int(input())
     result = 0
+    pan = [[0 for i in range(201)] for j in range(201)]
+    for i in range(201):
+        for j in range(201):
+            score = 11
+            p = (i*i+j*j)//400
+            if p>100:
+                continue
+            else:
+                pan[i][j] = 10-int(pow(p, 1/2))
     for i in range(N):
         x, y = map(int, input().split())
-        r2 = x*x + y*y
-        p = r2//400
-        # 10 -> 0~1
-        # 9 -> 1~4
-        # 8 -> 4~9....
-        score = pow(p,1/2)
-        result += (10-int(score))
+        result += pan[abs(x)][abs(y)]
     print(f'#{test_case} {int(result)}')
