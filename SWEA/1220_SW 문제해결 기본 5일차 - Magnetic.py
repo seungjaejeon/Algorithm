a46528775b16cb1774a56fff1804b5c1c8f82192
seungjaejeon <wjstmdwo2000@naver.com>
2023-10-13 13:20:02 +0900

    SWEA 1220 Magnetic

---
+++ SWEA/1220_SW 문제해결 기본 5일차 - Magnetic.py
@@ -0,0 +1,26 @@
+from collections import deque
+T = 10
+for test_case in range(1, T + 1):
+    N = int(input())
+    pan = []
+    for i in range(N):
+        line = list(map(int, input().split()))
+        pan.append(line)
+
+    q = deque()
+    result = 0
+    for i in range(N):
+        for j in range(N):
+            if pan[j][i]!=0:
+                q.append(pan[j][i])
+        flag = 0
+        for val in q:
+            if val==1 and flag==0:
+                flag=1       
+            if val==2 and flag==1:
+                result += 1
+                flag=0
+        print(f'#{test_case} {result}')
+    # 교착상태의 경우
+    # 한 라인에 N, S가 있을 때
+    # N이 위쪽, S가 아래쪽에 있을때
