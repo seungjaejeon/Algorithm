ea4f6f3bfa5416b1be651083ec0b108e5d230c96
seungjaejeon <wjstmdwo2000@naver.com>
2023-10-17 14:33:37 +0900

    SWEA 5948 새샘이의 745게임

--- SWEA/5948. 새샘이의 7-3-5 게임.py
+++ SWEA/5948. 새샘이의 7-3-5 게임.py
@@ -3,8 +3,8 @@ T = int(input())
 for test_case in range(1, T + 1):
     nums = list(map(int, input().split()))
     result = []
-    for i in combinations(range(7), 3):
-        result.append(nums[i[0]]+nums[i[1]]+nums[i[2]])
-    result_set = list(set(result))
-    result_set.sort(reverse=True)
-    print(f'#{test_case} {result_set[4]}')
+    for i in combinations(range(7), 3): # 0부터 6까지중 3가지를 고르는 경우
+        result.append(nums[i[0]]+nums[i[1]]+nums[i[2]]) # ex) i = (1,3,4)
+    result_set = list(set(result)) # 중복제거
+    result_set.sort(reverse=True) # 정렬
+    print(f'#{test_case} {result_set[4]}') # 5번째
