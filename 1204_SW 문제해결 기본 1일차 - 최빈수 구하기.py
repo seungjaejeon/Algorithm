4e6ac73db7c406febca1803c3758d61aac50b80b
seungjaejeon <wjstmdwo2000@naver.com>
2023-11-11 18:44:18 +0900

    1204. [S/W 문제해결 기본] 1일차 - 최빈수 구하기

---
+++ 1204_SW 문제해결 기본 1일차 - 최빈수 구하기.py
@@ -0,0 +1,18 @@
+T = int(input())
+for _ in range(1, T + 1):
+    test_case = int(input())
+    N = 1000
+    score = dict()
+    score_list = list(map(int, input().split()))
+    for i in range(N):
+        if score_list[i] in score.keys():
+            score[score_list[i]]+=1
+        else:
+            score[score_list[i]] = 1
+    max_count = 0
+    max_count_score = 0
+    for i in range(101):
+        if i in score.keys():
+            if max_count <= score[i]:
+                max_count_score = i
+                max_count = score[i]
