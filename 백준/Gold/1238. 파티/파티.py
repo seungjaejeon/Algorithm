5b023ee73c0083f410201f291b5f242541434a45
seungjae <52797720+seungjaejeon@users.noreply.github.com>
2023-12-06 22:33:05 +0900

    [Gold III] Title: 파티, Time: 76 ms, Memory: 34212 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -15,14 +15,19 @@ def dijkstra(s, edge):
                 heapq.heappush(q,(nxt,d+c))
     return dist
 
-n,m,x = map(int,input().split())
+n,m,x = map(int, sys.stdin.readline().split())
 graph = [[] for _ in range(n+1)]
 reverse_graph = [[] for _ in range(n+1)]
 for _ in range(m):
-    a,b,c = map(int,input().split())
-    graph[a].append((b,c))
-    reverse_graph[b].append((a,c))
-node2x = dijkstra(x,reverse_graph)
-x2node = dijkstra(x,graph)
+    start, end, c = map(int, sys.stdin.readline().split())
+    graph[start].append((end, c))
+    reverse_graph[end].append((start, c))
+back = dijkstra(x, reverse_graph)
+go = dijkstra(x, graph)
 
-print(max([x2node[i] + node2x[i] for i in range(1,n+1) if i != x]))
+answer = 0
+for i in range(1, n+1):
+    if i != x:
+        answer = max(answer, go[i] + back[i])
+
+print(answer)
