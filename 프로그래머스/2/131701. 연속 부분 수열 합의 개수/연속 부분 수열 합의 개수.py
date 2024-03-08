995954ab8634592d87c4c73d391033350a5b588f
seungjae <52797720+seungjaejeon@users.noreply.github.com>
2024-03-08 12:36:49 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 279.75 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,21 +1,11 @@
 def solution(elements):
-    answer = 0
-    N = len(elements)
-    for i in range(N):
-        elements.append(elements[i])
-    # 중복값 제외 set
-    # 수열 길이는 1부터 N까지 가능
-    # 7 9 1 1 4 7 9 1 1 4
-    result = set()
-    for i in range(1, N+1):
-        start = 0
-        end = start + i
-        while 1:
-            if end>2*N-1:
-                break
-            result.add(sum(elements[start:end]))
-            start += 1
-            end += 1
-            
-    answer = len(result)
-    return answer
+    ll = len(elements)
+    res = set()
+# 핵빠른코드
+    for i in range(ll):
+        ssum = elements[i]
+        res.add(ssum)
+        for j in range(i+1, i+ll):
+            ssum += elements[j%ll]
+            res.add(ssum)
+    return len(res)
